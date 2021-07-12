e0045c96be1dc32ec05cbee39650d110794a6be7
Pranam <68323012+prnam@users.noreply.github.com>
2021-07-12 09:42:10 +0530

    here is how to use enumerate

---
+++ source/list-example-two.py
@@ -0,0 +1,15 @@
+# Complete the skip_elements function to return every other element from the list,
+# using the enumerate function to check if an element is on an even position or an odd position.
+def skip_elements(elements):
+    # code goes here
+    new_list = []
+    for index, element in enumerate(elements):
+        if index % 2 == 0:
+            new_list.append(element)
+    return new_list
+
+
+# Should be ['a', 'c', 'e', 'g']
+print(skip_elements(["a", "b", "c", "d", "e", "f", "g"]))
+# Should be ['Orange', 'Strawberry', 'Peach']
+print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach']))
