ae96a2903e3b175608a40a6569c86a45f36e7763
Pranam <68323012+prnam@users.noreply.github.com>
2021-08-28 19:50:34 +0530

    add testcase for list-example-one.py

---
+++ source/list-example-one_test.py
@@ -0,0 +1,19 @@
+import importlib
+module = importlib.import_module("list-example-one")
+
+
+def test_skip_elements_with_list_of_chars():
+    testcase_input = ["a", "b", "c", "d", "e", "f", "g"]
+    expected_output = ['a', 'c', 'e', 'g']
+    assert module.skip_elements(testcase_input)== expected_output
+
+def test_skip_elements_with_list_of_words():
+    testcase_input = ['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach']
+    expected_output = ['Orange', 'Strawberry', 'Peach']
+    assert module.skip_elements(testcase_input)== expected_output
+
+def test_skip_elements_with_empty_list():
+    testcase_input = []
+    expected_output = []
+    assert module.skip_elements(testcase_input)== expected_output
+
