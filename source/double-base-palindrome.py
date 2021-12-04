bb309e99821b0cb393f4dab5b21c080ec93e4bc0
Pranam <68323012+prnam@users.noreply.github.com>
2021-12-04 18:34:16 +0530

    Create double-base-palindrome.py

---
+++ source/double-base-palindrome.py
@@ -0,0 +1,37 @@
+def getSumOfDoubleBasePalindrome(maximum):
+    result = 0
+    for number in range(1, maximum + 1):
+        output = isPalindrome(number)
+        result += output
+    return result
+
+
+def isPalindrome(number: int):
+    """
+    Checks for palindrome for given number for base 10 and 2
+    :param number:
+    :return:
+    """
+    reverse = 0
+    borrow_number = number
+    while borrow_number > 0:
+        digit = borrow_number % 10
+        reverse = reverse * 10 + digit
+        borrow_number = borrow_number // 10
+
+    if number == reverse:
+        binary = "{0:b}".format(number)
+        if binary[::-1] == binary:
+            return number
+        else:
+            return 0
+    else:
+        return 0
+
+
+if __name__ == '__main__':
+    maximum = int(input().strip())
+
+    result = getSumOfDoubleBasePalindrome(maximum)
+
+    print(result)
